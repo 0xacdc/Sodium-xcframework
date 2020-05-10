1bcd088fd7873ea0ee45cc8a97b25f3eb5176317
Alexander Mindov <38544113+0xacdc@users.noreply.github.com>
2020-05-10 13:39:29 +0200

    did optimize buid script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
+
+# delete previous version af xcframework
 rm -rf *.xcframework
+# read Module Name from Modulemap
+MODULE_NAME=$(sed -n -e 's/framework module\(.*\){/\1/p' module.modulemap)
 
 cd libsodium
 ./autogen.sh
@@ -12,28 +16,27 @@ lipo -remove i386 lib/libsodium.a -o libsodium-ios.a
 lipo -remove x86_64 libsodium-ios.a -o libsodium-ios.a
 
 function create_framework () {
-    PRODUCT_NAME="LibSodium"
-    FRAMEWORK_NAME="LibSodium.framework"
+    FRAMEWORK_NAME="${MODULE_NAME}.framework"
 
     # Create the path to the real Headers die
     mkdir -p "$1/${FRAMEWORK_NAME}/Versions/A/Headers"
     mkdir -p "$1/${FRAMEWORK_NAME}/Versions/A/Modules"
     # Copy the moduleMap and binary itself
     cp ../../module.modulemap "$1/${FRAMEWORK_NAME}/Versions/A/Modules"
-    cp -a ${PWD}/$2 "$1/${FRAMEWORK_NAME}/Versions/A/${PRODUCT_NAME}"
+    cp -a ${PWD}/$2 "$1/${FRAMEWORK_NAME}/Versions/A/${MODULE_NAME}"
     
     # Create the required symlinks
     ln -sfh A "$1/${FRAMEWORK_NAME}/Versions/Current"
     ln -sfh Versions/Current/Headers "$1/${FRAMEWORK_NAME}/Headers"
     ln -sfh Versions/Current/Modules "$1/${FRAMEWORK_NAME}/Modules"
-    ln -sfh "Versions/Current/${PRODUCT_NAME}" \
-                 "$1/${FRAMEWORK_NAME}/${PRODUCT_NAME}"
+    ln -sfh "Versions/Current/${MODULE_NAME}" \
+                 "$1/${FRAMEWORK_NAME}/${MODULE_NAME}"
 
     # Copy the public headers into the framework
     cp -r "include/" \
                "$1/${FRAMEWORK_NAME}/Versions/A/Headers"
 
-    mv "$1/${FRAMEWORK_NAME}/Versions/A/Headers/sodium.h" "$1/${FRAMEWORK_NAME}/Versions/A/Headers/${PRODUCT_NAME}.h"
+    mv "$1/${FRAMEWORK_NAME}/Versions/A/Headers/sodium.h" "$1/${FRAMEWORK_NAME}/Versions/A/Headers/${MODULE_NAME}.h"
 }
 
 for binary in `ls *.a`
@@ -49,7 +52,7 @@ xcodebuild -create-xcframework \
     -framework ios/${FRAMEWORK_NAME} \
     -framework sim/${FRAMEWORK_NAME} \
     -framework catalyst/${FRAMEWORK_NAME} \
-    -output ../../${PRODUCT_NAME}.xcframework
+    -output ../../${MODULE_NAME}.xcframework
     
 rm -rf lib
 
