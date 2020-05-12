9c64828da597d071e9abdd0c8b61012e4879ee45
Alexander Mindov <38544113+0xacdc@users.noreply.github.com>
2020-05-12 17:42:01 +0200

    update build script for whatchOS support

--- build.sh
+++ build.sh
@@ -8,29 +8,38 @@ parsed=$(sed -n -e 's/framework module\(.*\){/\1/p' module.modulemap)
 MODULE_NAME=$(echo ${parsed} | xargs)
 
 cd libsodium
-#./autogen.sh
+./autogen.sh
 
-#dist-build/ios.sh
-#dist-build/osx.sh
+# Run Build Scripts
+dist-build/ios.sh
+dist-build/osx.sh
+dist-build/watchos.sh
 
-
+# Create temorary dirrectory
 rm -rf tmp_xcframework
 mkdir tmp_xcframework
 
+# Separate iOS platforms
 lipo libsodium-ios/lib/libsodium.a -thin i386 -output tmp_xcframework/libsodium-sim.a
 lipo libsodium-ios/lib/libsodium.a -thin x86_64 -output tmp_xcframework/libsodium-catalyst.a
 lipo -remove i386 libsodium-ios/lib/libsodium.a -o tmp_xcframework/libsodium-ios.a
 lipo -remove x86_64 tmp_xcframework/libsodium-ios.a -o tmp_xcframework/libsodium-ios.a
 
+# Separate WatchOS Platform
+#Both watchos-x86_64-simulator and watchos-i386-simulator represent two equivalent library definitions.
+lipo libsodium-watchos/lib/libsodium.a -thin i386 -output tmp_xcframework/libsodium-watchosSim.a
+lipo -remove i386 libsodium-watchos/lib/libsodium.a -o tmp_xcframework/libsodium-watchos.a
+lipo -remove x86_64 tmp_xcframework/libsodium-watchos.a -o tmp_xcframework/libsodium-watchos.a
+
+#Copy OSX Platform
 cp libsodium-osx/lib/libsodium.a tmp_xcframework/libsodium-osx.a
-cp -r libsodium-ios/include tmp_xcframework/include
 
 cd tmp_xcframework
 
 function create_framework () {
     FRAMEWORK_NAME="${MODULE_NAME}.framework"
 
-    # Create the path to the real Headers die
+    # Create the path to the real Headers
     mkdir -p "$1/${FRAMEWORK_NAME}/Versions/A/Headers"
     mkdir -p "$1/${FRAMEWORK_NAME}/Versions/A/Modules"
     # Copy the moduleMap and binary itself
@@ -44,9 +53,18 @@ function create_framework () {
     ln -sfh "Versions/Current/${MODULE_NAME}" \
                  "$1/${FRAMEWORK_NAME}/${MODULE_NAME}"
 
-    # Copy the public headers into the framework
-    cp -r "include/" \
-               "$1/${FRAMEWORK_NAME}/Versions/A/Headers"
+    # Copy the headers into the framework
+    case $1 in
+         osx)
+              cp -r ../libsodium-osx/include/* "$1/${FRAMEWORK_NAME}/Versions/A/Headers"
+              ;;
+         watchos)
+              cp -r ../libsodium-watchos/include/* "$1/${FRAMEWORK_NAME}/Versions/A/Headers"
+              ;;
+         *)
+              cp -r ../libsodium-ios/include/* "$1/${FRAMEWORK_NAME}/Versions/A/Headers"
+              ;;
+    esac
 
     mv "$1/${FRAMEWORK_NAME}/Versions/A/Headers/sodium.h" "$1/${FRAMEWORK_NAME}/Versions/A/Headers/${MODULE_NAME}.h"
 }
@@ -65,11 +83,14 @@ xcodebuild -create-xcframework \
     -framework sim/${FRAMEWORK_NAME} \
     -framework catalyst/${FRAMEWORK_NAME} \
     -framework osx/${FRAMEWORK_NAME} \
+    -framework watchos/${FRAMEWORK_NAME} \
+    -framework watchosSim/${FRAMEWORK_NAME} \
     -output ../../${MODULE_NAME}.xcframework
     
-#rm -rf ../libsodium-ios
-#rm -rf ../libsodium-osx
-#rm -rf ../tmp_xcframework
+rm -rf ../libsodium-ios
+rm -rf ../libsodium-osx
+rm -rf ../libsodium-watchos
+rm -rf ../tmp_xcframework
 
 echo "DONE!"
 
